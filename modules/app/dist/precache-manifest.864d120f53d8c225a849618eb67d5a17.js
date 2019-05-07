5b96c69a07122ad9643e0d8c861dd0f374fdab20
Roberto Paredes <rdparedessalazar@gmail.com>
2019-05-07 13:05:58 -0500

    update gitignore and add dist folder

---
+++ modules/app/dist/precache-manifest.864d120f53d8c225a849618eb67d5a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ce5f8cbde9f5ebfd6da36e21f30da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6201099ef5b641aeb0a",
+    "url": "/static/css/main.d01c5e13.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "c6201099ef5b641aeb0a",
+    "url": "/static/js/main.e1e184cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
